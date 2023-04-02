944d9582171987c930fff50b5b836110aa7a6bd3
roSEV <rostlove247@gmail.com>
2023-04-02 12:38:18 +0400

    ti negr

---
+++ src/tanks/game/Main.java
@@ -0,0 +1,14 @@
+package tanks.game;
+import java.awt.event.ActionEvent;
+import java.awt.*;
+import javax.swing.*;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Game tanks = new Game();
+		tanks.start();
+
+	}
+
+}
